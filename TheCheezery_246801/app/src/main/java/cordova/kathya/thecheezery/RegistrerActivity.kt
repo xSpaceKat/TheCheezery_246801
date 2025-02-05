8b0ceb3824f940e2da3dc0f008947742f286c781
Katt <kathya.cordova246801@potros.itson.edu.mx>
2025-02-05 00:28:49 -0700

    ?

---
+++ TheCheezery_246801/app/src/main/java/cordova/kathya/thecheezery/RegistrerActivity.kt
@@ -0,0 +1,28 @@
+package cordova.kathya.thecheezery
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegistrerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_registrer)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val button: Button = findViewById(R.id.sign_in_c) as Button
+        button.setOnClickListener{
+            var intent: Intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
